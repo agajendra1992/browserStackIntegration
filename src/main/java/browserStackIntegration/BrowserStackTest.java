48da0755dfd924fcb9243e1374e86c44b3d9b4ad
gajendra.saxena <gajendra.akash02@gmail.com>
2024-06-17 15:06:57 +0530

    Initial commut for browserstack

---
+++ src/main/java/browserStackIntegration/BrowserStackTest.java
@@ -0,0 +1,42 @@
+package browserStackIntegration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BrowserStackTest {
+    public static final String USERNAME = "gajendrasaxena_drowfv";
+    public static final String AUTOMATE_KEY = "BkdGWcfdpjxqzt8R7Neh";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    @Test
+    public void loadBrowser() throws MalformedURLException {
+    	WebDriverManager.chromedriver().setup();
+    	 DesiredCapabilities caps = new DesiredCapabilities();
+         caps.setCapability("browserName", "Chrome");
+         caps.setCapability("browserVersion", "124.0");
+         
+         Map<String, Object> browserstackOptions = new HashMap<String, Object>();
+         browserstackOptions.put("os", "Windows");
+         browserstackOptions.put("osVersion", "11");
+        // browserstackOptions.put("sessionName", "My First Test"); 
+         // Typically the key should be 'sessionName' instead of 'name'
+         browserstackOptions.put("userName", USERNAME);
+         browserstackOptions.put("accessKey", AUTOMATE_KEY);
+
+         caps.setCapability("bstack:options", browserstackOptions);
+
+         WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+         driver.get("http://www.google.com");
+         System.out.println(driver.getTitle());
+         driver.quit();
+    }
+}
